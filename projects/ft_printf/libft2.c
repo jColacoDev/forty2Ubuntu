23b5d9d6283132d854c6bf0eb208d030ef0ad50e
jcolacodev <signup@jcolaco.dev>
2024-05-28 00:15:22 +0100

    refactor1

--- projects/ft_printf/libft2.c
+++ projects/ft_printf/libft2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joao-rde <joao-rde@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: joao-rde <joao-rde@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 17:29:00 by joao-rde          #+#    #+#             */
-/*   Updated: 2024/05/24 20:16:43 by joao-rde         ###   ########.fr       */
+/*   Updated: 2024/05/28 00:13:23 by joao-rde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ char	*ft_strdup(const char *s)
 	return (dup);
 }
 
-static int	ft_count_digits_base(unsigned long long n, int base)
+int	ft_count_digits_base(unsigned long long n, int base)
 {
 	int	count;
 
@@ -52,7 +52,8 @@ static int	ft_count_digits_base(unsigned long long n, int base)
 	return (count);
 }
 
-char	*ft_utoa_base(unsigned long long n, int base, char *charset, t_flags flags)
+char	*ft_utoa_base(unsigned long long n, int base, char *charset,
+		t_flags flags)
 {
 	int		len;
 	char	*str;
@@ -65,8 +66,8 @@ char	*ft_utoa_base(unsigned long long n, int base, char *charset, t_flags flags)
 		return (NULL);
 	str[len] = '\0';
 	if (n == 0 && !flags.precision)
-        str[0] = ' ';
-	else if (n == 0 )
+		str[0] = ' ';
+	else if (n == 0)
 		str[0] = charset[0];
 	while (n > 0)
 	{
@@ -74,31 +75,4 @@ char	*ft_utoa_base(unsigned long long n, int base, char *charset, t_flags flags)
 		n /= base;
 	}
 	return (str);
-}
-
-char *ft_itoa(int n, t_flags flags)
-{
-    int len;
-    char *str;
-    unsigned int num;
-
-    num = (n < 0) ? -n : n; // Use absolute value of n
-    len = (n == 0) ? 1 : ft_count_digits_base(num, 10);
-    len += (n < 0) ? 1 : 0; // Add 1 for '-' sign if n is negative
-    str = (char *)malloc(sizeof(char) * (len + 1));
-    if (!str)
-        return (NULL);
-    str[len] = '\0';
-    if (n == 0 && !flags.precision)
-        str[0] = ' ';
-	else if (n == 0 )
-        str[0] = '0';
-    while (num > 0)
-    {
-        str[--len] = (num % 10) + '0';
-        num /= 10;
-    }
-    if (n < 0)
-        str[0] = '-';
-    return (str);
 }
--- projects/ft_printf/libft3.c
+++ projects/ft_printf/libft3.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft3.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joao-rde <joao-rde@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: joao-rde <joao-rde@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 17:29:00 by joao-rde          #+#    #+#             */
-/*   Updated: 2024/05/21 19:03:14 by joao-rde         ###   ########.fr       */
+/*   Updated: 2024/05/28 00:14:23 by joao-rde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,133 +48,192 @@ void	*ft_memset(void *s, int c, size_t n)
 	return (s);
 }
 
-
-unsigned long ft_strtoul_base(const char *str, const char *base)
+void	ft_strtoul_base_aux(unsigned long *result, int i,
+		const char *str, const char *base)
 {
-    unsigned long result = 0;
-    int base_len = ft_strlen(base);
-    int sign = 1;
-    int i = 0;
+	int	j;
+	int	base_len;
 
-    // Handle leading whitespace
-    while (ft_isspace(str[i]))
-        i++;
-
-    // Handle sign
-    if (str[i] == '-' || str[i] == '+') {
-        sign = (str[i] == '-') ? -1 : 1;
-        i++;
-    }
-
-    // Calculate result
-    while (str[i]) {
-        int j = 0;
-        while (base[j] && str[i] != base[j])
-            j++;
-        if (!base[j])
-            break;
-        result = result * base_len + j;
-        i++;
-    }
-
-    return sign * result;
+	base_len = ft_strlen(base);
+	while (str[i])
+	{
+		j = 0;
+		while (base[j] && str[i] != base[j])
+			j++;
+		if (!base[j])
+			break ;
+		*result = *result * base_len + j;
+		i++;
+	}
 }
 
-int ft_isspace(int c)
+unsigned long	ft_strtoul_base(const char *str, const char *base)
 {
-    return (c == ' ' || c == '\t' || c == '\n'
-            || c == '\v' || c == '\f' || c == '\r');
+	unsigned long	result;
+	int				sign;
+	int				i;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		sign = 1;
+		if ((str[i] == '-'))
+			sign = -1;
+		i++;
+	}
+	ft_strtoul_base_aux(&result, i, str, base);
+	return (sign * result);
 }
 
-static int ft_get_value(char c, const char *base)
+int	ft_isspace(int c)
 {
-    int i = 0;
-    while (base[i])
-    {
-        if (base[i] == c)
-            return i;
-        i++;
-    }
-    return -1; // Return -1 for invalid characters
+	return (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f'
+		|| c == '\r');
 }
 
-int ft_atoi_base(const char *str, const char *base)
+static int	ft_get_value(char c, const char *base)
 {
-    int result = 0;
-    int sign = 1;
-    int base_len = ft_strlen(base);
-    int i = 0;
+	int	i;
 
-    // Handle leading whitespace
-    while (ft_isspace(str[i]))
-        i++;
-
-    // Handle sign
-    if (str[i] == '-' || str[i] == '+') {
-        sign = (str[i] == '-') ? -1 : 1;
-        i++;
-    }
-
-    // Calculate result
-    while (str[i]) {
-        int value = ft_get_value(str[i], base);
-        if (value == -1)
-            break; // Invalid character, stop parsing
-        result = result * base_len + value;
-        i++;
-    }
-
-    return sign * result;
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
 
-char *ft_strchr(const char *s, int c) 
+int	ft_atoi_base(const char *str, const char *base)
 {
-    while (*s != '\0') {
-        if (*s == (char)c) {
-            return (char *)s;
-        }
-        s++;
-    }
-    if (c == '\0') {
-        return (char *)s;
-    }
-    return NULL;
+	int	result;
+	int	sign;
+	int	base_len;
+	int	i;
+	int	value;
+
+	result = 0;
+	sign = 1;
+	base_len = ft_strlen(base);
+	i = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		sign = (str[i] == '-') ? -1 : 1;
+		i++;
+	}
+	while (str[i])
+	{
+		value = ft_get_value(str[i], base);
+		if (value == -1)
+			break ;
+		result = result * base_len + value;
+		i++;
+	}
+	return (sign * result);
 }
 
-
-char *ft_concat(const char *str1, const char *str2, int at_beginning)
+char	*ft_strchr(const char *s, int c)
 {
-    size_t len1 = 0, len2 = 0;
-    while (str1[len1] != '\0') len1++;
-    while (str2[len2] != '\0') len2++;
+	while (*s != '\0')
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
 
-    // Allocate memory for concatenated string
-    char *result = (char *)malloc((len1 + len2 + 1) * sizeof(char));
-    if (result == NULL) {
-        // fprintf(stderr, "Memory allocation failed\n");
-        exit(1);
-    }
+char	*ft_concat(const char *str1, const char *str2, int at_beginning)
+{
+	size_t	len1;
+	size_t	len2;
+	char	*result;
 
-    // Concatenate strings
-    if (at_beginning) {
-        // Copy str2 followed by str1
-        for (size_t i = 0; i < len2; i++) {
-            result[i] = str2[i];
-        }
-        for (size_t i = 0; i < len1; i++) {
-            result[i + len2] = str1[i];
-        }
-        result[len1 + len2] = '\0'; // Null-terminate the string
-    } else {
-        // Copy str1 followed by str2
-        for (size_t i = 0; i < len1; i++) {
-            result[i] = str1[i];
-        }
-        for (size_t i = 0; i < len2; i++) {
-            result[i + len1] = str2[i];
-        }
-        result[len1 + len2] = '\0'; // Null-terminate the string
-    }
+	len1 = 0;
+	len2 = 0;
+	while (str1[len1] != '\0')
+		len1++;
+	while (str2[len2] != '\0')
+		len2++;
+	result = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+	if (result == NULL)
+	{
+		exit(1);
+	}
+	if (at_beginning)
+	{
+		for (size_t i = 0; i < len2; i++)
+		{
+			result[i] = str2[i];
+		}
+		for (size_t i = 0; i < len1; i++)
+		{
+			result[i + len2] = str1[i];
+		}
+		result[len1 + len2] = '\0';
+	}
+	else
+	{
+		for (size_t i = 0; i < len1; i++)
+		{
+			result[i] = str1[i];
+		}
+		for (size_t i = 0; i < len2; i++)
+		{
+			result[i + len1] = str2[i];
+		}
+		result[len1 + len2] = '\0';
+	}
+	return (result);
+}
 
-    return result;
+char	*ft_itoa_aux(char *str, int *len, int n, t_flags flags)
+{
+	unsigned int	num;
+
+	num = n;
+	if (n < 0)
+		num = -n;
+	if (n == 0 && !flags.precision)
+		str[0] = ' ';
+	else if (n == 0)
+		str[0] = '0';
+	while (num > 0)
+	{
+		str[--(*len)] = (num % 10) + '0';
+		num /= 10;
+	}
+	if (n < 0)
+		str[0] = '-';
+	return (str);
+}
+char	*ft_itoa(int n, t_flags flags)
+{
+	int				len;
+	char			*str;
+	unsigned int	num;
+
+	num = n;
+	if (n < 0)
+		num = -n;
+	len = ft_count_digits_base(num, 10);
+	if (n == 0)
+		len = 1;
+	else if (n < 0)
+		len += 1;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	ft_itoa_aux(str, &len, n, flags);
+	return (str);
 }
